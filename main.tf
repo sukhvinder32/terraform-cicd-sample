87cbec74b65f32911a8af6bcb4015f22dd939b69
Dabeer Shaikh <dab8106@gmail.com>
2023-11-16 01:17:32 +0530

    updated main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "tfdemo16112023"
+    bucket         = "tf16112023"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
