f0903709453c3e2685f9610a4cd099747d57ad0f
sukh <sukhvinder.bajwa@gmail.com>
2024-05-26 12:44:27 +0100

    Updated S3 bucket

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "tf16112023"
+    bucket         = "tf25052024"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
